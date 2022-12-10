beceb35bbd0516f793c3822d6cfe3dd59564961f
Chris Gitmed <chris@Chriss-MacBook-Pro.local>
2022-12-10 13:03:54 -0800

    Completed day 2, part 2

--- advent/2/_.test.js
+++ advent/2/_.test.js
@@ -1,12 +1,12 @@
 const { test, expect } = require('@jest/globals');
 
-const { getTotalScore, getNewTotalScore } = require('./solutions');
+const { getTotalScore, getTotalScore2 } = require('./solutions');
 const inputData = require('./input');
 
-test('Should return the max amount of calories on one elf.', () => {
+test('Should return the total score if everything goes according to the strategy guide.', () => {
   expect(getTotalScore(inputData)).toBe(15691);
 });
 
-// test('Should return the sum of of calories on the three elves with the most calories.', () => {
-//   expect(getThreeHighestCalories(inputData)).toBe(213958);
-// });
+test('Using the new rules, should return the total score if everything goes according to the strategy guide.', () => {
+  expect(getTotalScore2(inputData)).toBe(12989);
+});
