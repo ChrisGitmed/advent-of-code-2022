2f02265d915aa560871187b3ed5e4def47dfb4ac
Chris Gitmed <chris@Chriss-MacBook-Pro.local>
2022-12-10 11:53:26 -0800

    Refactored solutions for day 1

--- advent/1/solutions.js
+++ advent/1/solutions.js
@@ -1,26 +1,18 @@
 // Part 1
-const getHighestCalories = (data) => {
-  const firstSplit = data.split('\n\n');
-  const secondSplit = firstSplit.map((item) => item.split('\n').map((str) => parseInt(str, 10)));
-  
-  let max = -Infinity;
-  for (const elf of secondSplit) {
-    const sumOfCalories = elf.reduce((a, b) => a + b, 0);
-    if (sumOfCalories > max) max = sumOfCalories;
-  }
-  return max;
-};
+const getHighestCalories = (data) => data.split('\n\n')
+  .map((item) => item.split('\n')
+  .map((str) => parseInt(str, 10)))
+  .map((numArr) => numArr.reduce((a, b) => a + b, 0))
+  .sort((a, b) => b - a)[0];
 
 // Part 2
-const getThreeHighestCalories = (data) => {
-  return data
-    .split('\n\n')
-    .map((item) => item.split('\n')
-    .map((strArr) => parseInt(strArr, 10)))
-    .map((numArr) => numArr.reduce((a, b) => a + b, 0))
-    .sort((a, b) => b - a)
-    .slice(0,3)
-    .reduce((a, b) => a + b, 0)
-};
+const getThreeHighestCalories = (data) => data
+  .split('\n\n')
+  .map((item) => item.split('\n')
+  .map((strArr) => parseInt(strArr, 10)))
+  .map((numArr) => numArr.reduce((a, b) => a + b, 0))
+  .sort((a, b) => b - a)
+  .slice(0,3)
+  .reduce((a, b) => a + b, 0);
 
 module.exports = { getHighestCalories, getThreeHighestCalories };
