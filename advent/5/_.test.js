b1e596432335ea3f6cb9e7b430697f4b6a45fa9a
Chris Gitmed <chris@Chriss-MacBook-Pro.local>
2022-12-13 19:01:08 -0800

    Completed Day 5, part 1

--- advent/5/_.test.js
+++ advent/5/_.test.js
@@ -3,9 +3,8 @@ const { test, expect } = require('@jest/globals');
 const { getTopCratesAfterArrangement } = require('./solutions');
 const { inputData } = require('./input');
 
-const testInput = 'move 1 from 1 to 2'
 test('Should return the top crates after arrangement', () => {
-  expect(getTopCratesAfterArrangement(testInput)).toBe('CFHZNPMND');
+  expect(getTopCratesAfterArrangement(inputData)).toBe('SPFMVDTZT');
 });
 
 // test('', () => {
--- advent/5/solutions.js
+++ advent/5/solutions.js
@@ -1,84 +1,40 @@
 const { inputData } = require("./input");
 
-// const stacks = [
-//   [],
-//   ['W', 'T', 'H', 'P', 'J', 'C', 'F'],
-//   ['H', 'B', 'J', 'Z', 'F', 'V', 'R', 'G'],
-//   ['R', 'T', 'P', 'H'],
-//   ['T', 'H', 'P', 'N', 'S', 'Z'],
-//   ['D', 'C', 'J', 'H', 'Z', 'F', 'V', 'N'],
-//   ['Z', 'D', 'W', 'F', 'G', 'M', 'P'],
-//   ['P', 'D', 'J', 'S', 'W', 'Z', 'V', 'M'],
-//   ['S', 'D', 'N'],
-//   ['M', 'F', 'S', 'Z', 'D']
-// ];
-
 // Part 1
 const getTopCratesAfterArrangement = (input) => {
   const stacks = [
     [],
-    ['W', 'T', 'H', 'P', 'J', 'C', 'F'],
-    ['H', 'B', 'J', 'Z', 'F', 'V', 'R', 'G'],
-    ['R', 'T', 'P', 'H'],
-    ['T', 'H', 'P', 'N', 'S', 'Z'],
-    ['D', 'C', 'J', 'H', 'Z', 'F', 'V', 'N'],
-    ['Z', 'D', 'W', 'F', 'G', 'M', 'P'],
-    ['P', 'D', 'J', 'S', 'W', 'Z', 'V', 'M'],
-    ['S', 'D', 'N'],
-    ['M', 'F', 'S', 'Z', 'D']
+    ['F', 'C', 'J', 'P', 'H', 'T', 'W'],
+    ['G', 'R', 'V', 'F', 'Z', 'J', 'B', 'H'],
+    ['H', 'P', 'T', 'R'],
+    ['Z', 'S', 'N', 'P', 'H', 'T'],
+    ['N', 'V', 'F', 'Z', 'H', 'J', 'C', 'D'],
+    ['P', 'M', 'G', 'F', 'W', 'D', 'Z'],
+    ['M', 'V', 'Z', 'W', 'S', 'J', 'D', 'P'],
+    ['N', 'D', 'S'],
+    ['D', 'Z', 'S', 'F', 'M']
   ];
 
   const instructions = input.split('\n');
 
-  let elementToMove = '';
-  let idx = 0;
-  while (elementToMove !== undefined) {
-    const instruction = instructions[idx];
+  for (const instruction of instructions) {
     const [firstHalf, secondHalf] = instruction.split(' from ');
-    const moveCount = Number(firstHalf.slice(-1));
+    const [,moveStr] = firstHalf.split(' ');
+    const moveCount = Number(moveStr);
     const startingStack = Number(secondHalf[0]);
     const destinationStack = Number(secondHalf.slice(-1));
 
-    console.log('moveCount: ', moveCount);
-    console.log('startingStack: ', startingStack);
-    console.log('destinationStack: ', destinationStack);    
-    console.log('stacks: ', stacks)
     for (let i = 0; i < moveCount; i++) {
-      // console.log('moved')
-      elementToMove = stacks[startingStack].pop();
+      const elementToMove = stacks[startingStack].pop();
       stacks[destinationStack].push(elementToMove);
-      // console.log('stacks: ', stacks)
     }
-    idx += 1;
   }
 
-
-
-  // for (const instruction of instructions) {
-  //   const [firstHalf, secondHalf] = instruction.split(' from ');
-  //   const moveCount = Number(firstHalf.slice(-1));
-  //   const startingStack = Number(secondHalf[0]);
-  //   const destinationStack = Number(secondHalf.slice(-1));
-
-  //   for (let i = 0; i < moveCount; i++) {
-  //     const elementToMove = stacks[startingStack].pop();
-  //     if (elementToMove === undefined) {
-  //       console.log('stacks: ', stacks)
-  //       console.log('moveCount: ', moveCount);
-  //       console.log('startingStack: ', startingStack);
-  //       console.log('destinationStack: ', destinationStack);
-  //     }
-  //     stacks[destinationStack].push(elementToMove);
-  //     // console.log('stacks[destination]: ', stacks[destination]);
-  //   }
-  // }
-
-  console.log('stacks: ', stacks)
-  let answer = '';
+  let topCrates = '';
   for (let x = 1; x < stacks.length; x++) {
-    answer += stacks[x][stacks[x].length - 1];
+    topCrates += stacks[x][stacks[x].length - 1];
   }
-  return answer;
+  return topCrates;
 }
 
 // Part 2
