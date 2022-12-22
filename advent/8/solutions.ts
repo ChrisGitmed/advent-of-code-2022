b023884ce3ec8fecdfc1e2baf9d6d773227b1b46
Chris Gitmed <chris@Chriss-MacBook-Pro.local>
2022-12-21 16:46:41 -0800

    Cleanup advent/8

--- advent/8/solutions.ts
+++ advent/8/solutions.ts
@@ -1,62 +1,52 @@
-import { inputData } from './input.js';
-const testInput = `30373
-25512
-65332
-33549
-35390`;
-
 // Part 1
 export const countVisibleTrees = (input: string) => {
-  console.log('input:\n', input, '\n');
   const rows = input.split('\n');
   let countOfVisibleTrees = 0;
 
   // Loop over each row
   rows.forEach((currentRow: string, rowNum: number) => {
-    console.log('currentRow: ', currentRow)
     for (let treeIdx = 0; treeIdx < currentRow.length; treeIdx++) {
-      // const currentTree:number = parseInt(currentRow[treeIdx], 10);
       const currentTree:string = currentRow[treeIdx];
-      console.log('currentTree: ', currentTree);
 
       // Look 'North'
       let isVisibleFromNorth = true;
-      // console.log('rowNum: ', rowNum);
-      // console.log('treeIdx: ', treeIdx);
-      for (let prevRowNum = rowNum; prevRowNum > 0; prevRowNum--) {
-        // console.log('prevRowNum: ', prevRowNum)
+      for (let prevRowNum = rowNum - 1; prevRowNum >= 0; prevRowNum--) {
+        const previousRow = rows[prevRowNum];
+        const compareTree = previousRow[treeIdx];
         if (
-          rows[prevRowNum - 1][treeIdx] >= currentTree &&
-          rows[prevRowNum - 1][treeIdx] !== '0'
+          compareTree >= currentTree &&
+          compareTree !== '0'
         ) isVisibleFromNorth = false;
       }
 
       // Look 'East'
       let isVisibleFromEast = true
       for (let eastTreeIdx = treeIdx + 1; eastTreeIdx < currentRow.length; eastTreeIdx++) {
+        const compareTree = currentRow[eastTreeIdx];
         if (
-          currentRow[eastTreeIdx] >= currentTree &&
-          currentRow[eastTreeIdx] !== '0'
+          compareTree >= currentTree &&
+          compareTree !== '0'
         ) isVisibleFromEast = false;
       }
 
       // Look 'West'
       let isVisibleFromWest = true
       for (let westTreeIdx = treeIdx - 1; westTreeIdx >= 0; westTreeIdx--) {
-        // console.log('west tree: ', currentRow[westTreeIdx])
-        
+        const compareTree = currentRow[westTreeIdx];
         if (
-          currentRow[westTreeIdx] >= currentTree &&
-          currentRow[westTreeIdx] !== '0'
+          compareTree >= currentTree &&
+          compareTree !== '0'
         ) isVisibleFromWest = false;
       }
 
       // Look 'South'
       let isVisibleFromSouth = true;
       for (let nextRowNum = rowNum + 1; nextRowNum < rows.length; nextRowNum++) {
+        const nextRow = rows[nextRowNum];
+        const compareTree = nextRow[treeIdx];
         if (
-          rows[nextRowNum][treeIdx] >= currentTree &&
-          rows[nextRowNum][treeIdx] !== '0'
+          compareTree >= currentTree &&
+          compareTree !== '0'
         ) isVisibleFromSouth = false;
       }
 
@@ -69,5 +59,5 @@ export const countVisibleTrees = (input: string) => {
 
 // Part 2
 
-console.log('answer: ', countVisibleTrees(testInput)) // 21
-console.log('answer: ', countVisibleTrees(inputData)) // 
+// console.log('answer: ', countVisibleTrees(testInput)) // 21
+// console.log('answer: ', countVisibleTrees(inputData)) // 
--- dist/8/solutions.js
+++ dist/8/solutions.js
@@ -1,51 +1,43 @@
-import { inputData } from './input.js';
-const testInput = `30373
-25512
-65332
-33549
-35390`;
 // Part 1
 export const countVisibleTrees = (input) => {
-    console.log('input:\n', input, '\n');
     const rows = input.split('\n');
     let countOfVisibleTrees = 0;
     // Loop over each row
     rows.forEach((currentRow, rowNum) => {
-        console.log('currentRow: ', currentRow);
         for (let treeIdx = 0; treeIdx < currentRow.length; treeIdx++) {
-            // const currentTree:number = parseInt(currentRow[treeIdx], 10);
             const currentTree = currentRow[treeIdx];
-            console.log('currentTree: ', currentTree);
             // Look 'North'
             let isVisibleFromNorth = true;
-            // console.log('rowNum: ', rowNum);
-            // console.log('treeIdx: ', treeIdx);
-            for (let prevRowNum = rowNum; prevRowNum > 0; prevRowNum--) {
-                // console.log('prevRowNum: ', prevRowNum)
-                if (rows[prevRowNum - 1][treeIdx] >= currentTree &&
-                    rows[prevRowNum - 1][treeIdx] !== '0')
+            for (let prevRowNum = rowNum - 1; prevRowNum >= 0; prevRowNum--) {
+                const previousRow = rows[prevRowNum];
+                const compareTree = previousRow[treeIdx];
+                if (compareTree >= currentTree &&
+                    compareTree !== '0')
                     isVisibleFromNorth = false;
             }
             // Look 'East'
             let isVisibleFromEast = true;
             for (let eastTreeIdx = treeIdx + 1; eastTreeIdx < currentRow.length; eastTreeIdx++) {
-                if (currentRow[eastTreeIdx] >= currentTree &&
-                    currentRow[eastTreeIdx] !== '0')
+                const compareTree = currentRow[eastTreeIdx];
+                if (compareTree >= currentTree &&
+                    compareTree !== '0')
                     isVisibleFromEast = false;
             }
             // Look 'West'
             let isVisibleFromWest = true;
             for (let westTreeIdx = treeIdx - 1; westTreeIdx >= 0; westTreeIdx--) {
-                // console.log('west tree: ', currentRow[westTreeIdx])
-                if (currentRow[westTreeIdx] >= currentTree &&
-                    currentRow[westTreeIdx] !== '0')
+                const compareTree = currentRow[westTreeIdx];
+                if (compareTree >= currentTree &&
+                    compareTree !== '0')
                     isVisibleFromWest = false;
             }
             // Look 'South'
             let isVisibleFromSouth = true;
             for (let nextRowNum = rowNum + 1; nextRowNum < rows.length; nextRowNum++) {
-                if (rows[nextRowNum][treeIdx] >= currentTree &&
-                    rows[nextRowNum][treeIdx] !== '0')
+                const nextRow = rows[nextRowNum];
+                const compareTree = nextRow[treeIdx];
+                if (compareTree >= currentTree &&
+                    compareTree !== '0')
                     isVisibleFromSouth = false;
             }
             const isVisible = isVisibleFromNorth || isVisibleFromEast || isVisibleFromSouth || isVisibleFromWest;
@@ -56,5 +48,5 @@ export const countVisibleTrees = (input) => {
     return countOfVisibleTrees;
 };
 // Part 2
-console.log('answer: ', countVisibleTrees(testInput)); // 21
-console.log('answer: ', countVisibleTrees(inputData)); // 
+// console.log('answer: ', countVisibleTrees(testInput)) // 21
+// console.log('answer: ', countVisibleTrees(inputData)) // 
