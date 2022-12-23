39ba5d8a0453ada5cb5f44ebea0afb3533faadcf
Chris Gitmed <chris@Chriss-MBP.home>
2022-12-22 20:39:15 -0700

    Completed day 8

--- advent/8/_.test.js
+++ advent/8/_.test.js
@@ -1,12 +1,12 @@
 const { test, expect } = require('@jest/globals');
 
-const { countVisibleTrees } = require('./solutions.js');
+const { countVisibleTrees, getLargestScenicScore } = require('./solutions.js');
 const { inputData } = require('./input.js');
 
 test('Should return the count of all visible trees', () => {
-  expect(countVisibleTrees(inputData)).toBe(1835); // 1835
+  expect(countVisibleTrees(inputData)).toBe(1835);
 });
 
-// test('', () => {
-//   expect((inputData)).toBe();
-// });
+test('Should return the largest scenic score', () => {
+  expect(getLargestScenicScore(inputData)).toBe(263670);
+});
